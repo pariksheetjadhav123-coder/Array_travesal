7e1608b3b5c1d3e71f18562696abe204d18b9799
pariksheetjadhav123-coder <pariksheetjadhav123@gmail.com>
2026-09-23 14:13:00 +0530

    Add files via upload

---
+++ Array_travesal.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = 5;
+
+    cout << "Array elements: ";
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
